c4ef99637888ee2e2fe6a8b3f22bdc50ae8e0632
Krishna Indani <indanikrishna@gmail.com>
2020-12-15 20:21:29 -0800

    day 4 part one

---
+++ 2020/day4/part1/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var mandatoryFields = []string{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
+
+func main() {
+	data := readFile()
+	fmt.Println("data", data)
+
+	passports := parsePassport(data)
+	validPassports := 0
+	for _, p := range passports {
+		valid := checkValidPassports(p)
+		if valid {
+			validPassports++
+		}
+	}
+	fmt.Println("validPassports", validPassports)
+}
+
+func checkValidPassports(passport string) bool {
+	for _, field := range mandatoryFields {
+		if !strings.Contains(passport, field) {
+			return false
+		}
+	}
+	return true
+}
+
+func parsePassport(passports []string) []string {
+	parsedPassports := []string{}
+	passport := ""
+	for _, p := range passports {
+		if len(p) > 0 {
+			passport += p
+		} else {
+			parsedPassports = append(parsedPassports, passport)
+			passport = ""
+		}
+	}
+	return parsedPassports
+}
+
+func readFile() []string {
+	var input []string
+
+	file, _ := os.Open("./data.txt")
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		input = append(input, scanner.Text())
+	}
+	input = append(input, "")
+	return input
+}
