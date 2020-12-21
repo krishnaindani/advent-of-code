fa50de9f79fb50e48f467ed1c28c8260f4dc912f
Krishna Indani <indanikrishna@gmail.com>
2020-12-20 21:27:19 -0800

    day 4 part 2 make it work

--- 2020/day4/part2/main.go
+++ 2020/day4/part2/main.go
@@ -4,34 +4,106 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
 var mandatoryFields = []string{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
 
+var (
+	birthYear      = "byr"
+	issueYear      = "iyr"
+	expirationyear = "eyr"
+	height         = "hgt"
+	hairColor      = "hcl"
+	eyeColor       = "ecl"
+	passportID     = "pid"
+)
+
 func main() {
 	data := readFile()
-	fmt.Println("data", data)
 
 	passports := parsePassport(data)
-	validPassports := 0
-	for _, p := range passports {
-		fmt.Println("p", p)
-		valid := checkValidPassports(p)
-		if valid {
-			validPassports++
-		}
-	}
-	fmt.Println("validPassports", validPassports)
+
+	validPassportsCount := validPassports(passports)
+
+	fmt.Println("validPassports", validPassportsCount)
 }
 
-func checkValidPassports(passport string) bool {
+func validPassports(passports []string) int {
+	fmt.Println("lenght", len(passports))
+	var validPassports int
+	for _, p := range passports {
+		validCheck := validPassportFieldContains(p)
+		if validCheck {
+			valid := validPassport(p)
+			if valid {
+				validPassports++
+			}
+		}
+	}
+	return validPassports
+}
+
+func validPassportFieldContains(passport string) bool {
 	for _, field := range mandatoryFields {
 		if !strings.Contains(passport, field) {
 			return false
 		}
 	}
+	return true
+}
 
+func validPassport(passport string) bool {
+	passportFields := strings.Split(passport, " ")
+	for _, f := range passportFields {
+		fields := strings.Split(f, ":")
+		if len(f) > 2 {
+			field := fields[0]
+			value := fields[1]
+			switch field {
+			case birthYear:
+				y, _ := strconv.Atoi(value)
+				valid := birthYearValidation(y)
+				if !valid {
+					return false
+				}
+			case issueYear:
+				y, _ := strconv.Atoi(value)
+				valid := issueYearValidation(y)
+				if !valid {
+					return false
+				}
+			case expirationyear:
+				y, _ := strconv.Atoi(value)
+				valid := expirationYearValidation(y)
+				if !valid {
+					return false
+				}
+			case height:
+				valid := heightValidation(value)
+				if !valid {
+					return false
+				}
+			case hairColor:
+				valid := hairColorValidation(value)
+				if !valid {
+					return false
+				}
+			case eyeColor:
+				valid := eyeColorValiadation(value)
+				if !valid {
+					return false
+				}
+			case passportID:
+				valid := passportIDValidation(value)
+				if !valid {
+					return false
+				}
+			}
+		}
+	}
 	return true
 }
 
@@ -41,6 +113,7 @@ func parsePassport(passports []string) []string {
 	for _, p := range passports {
 		if len(p) > 0 {
 			passport += p
+			passport += " "
 		} else {
 			parsedPassports = append(parsedPassports, passport)
 			passport = ""
@@ -52,7 +125,7 @@ func parsePassport(passports []string) []string {
 func readFile() []string {
 	var input []string
 
-	file, _ := os.Open("./sampleData.txt")
+	file, _ := os.Open("./data.txt")
 	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
@@ -60,4 +133,47 @@ func readFile() []string {
 	}
 	input = append(input, "")
 	return input
+}
+
+func birthYearValidation(birthYear int) bool {
+	return ((birthYear >= 1920) && (birthYear <= 2002))
+}
+
+func issueYearValidation(issueYear int) bool {
+	return ((issueYear >= 2010) && (issueYear <= 2020))
+}
+
+func expirationYearValidation(expirationYear int) bool {
+	return ((expirationYear >= 2020) && (expirationYear <= 2030))
+}
+
+func eyeColorValiadation(color string) bool {
+
+	return (color == "amb") || (color == "blu") || (color == "gry") ||
+		(color == "brn") || (color == "hzl") || (color == "oth") ||
+		(color == "grn")
+}
+
+func passportIDValidation(id string) bool {
+	return len(id) == 9
+}
+
+func hairColorValidation(color string) bool {
+	match, _ := regexp.MatchString("^#([0-9a-f]{6})", color)
+	return match
+}
+
+func heightValidation(height string) bool {
+
+	if strings.Contains(height, "cm") {
+		value, _ := strconv.Atoi(strings.Split(height, "cm")[0])
+		return (value >= 150) && (value <= 193)
+	}
+
+	if strings.Contains(height, "in") {
+		value, _ := strconv.Atoi(strings.Split(height, "in")[0])
+		return (value >= 59) && (value <= 76)
+	}
+
+	return false
 }
