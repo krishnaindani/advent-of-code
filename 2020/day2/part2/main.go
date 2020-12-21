47bbddb05a8cb3af6afb18c7259aa54996c5fb3b
Krishna Indani <indanikrishna@gmail.com>
2020-12-20 18:12:23 -0800

    refactor day2 part2

--- 2020/day2/part2/main.go
+++ 2020/day2/part2/main.go
@@ -17,19 +17,8 @@ func main() {
 func validPasswords(data []string) int {
 	var validPasswords int
 	for _, v := range data {
-		s := strings.Fields(v)
-
-		digitsSlice := strings.Split(s[0], "-")
-
-		minDigits, _ := strconv.Atoi(digitsSlice[0])
-		maxDigits, _ := strconv.Atoi(digitsSlice[1])
-		alphabetSupported := strings.Split(s[1], ":")[0]
-		passwordString := s[2]
-
-		minWord := string(passwordString[minDigits-1]) == alphabetSupported
-		maxWord := string(passwordString[maxDigits-1]) == alphabetSupported
-
-		if minWord != maxWord {
+		isValid := validPassword(v)
+		if isValid {
 			validPasswords++
 		}
 	}
@@ -37,10 +26,30 @@ func validPasswords(data []string) int {
 	return validPasswords
 }
 
+func validPassword(password string) bool {
+
+	s := strings.Fields(password)
+
+	digitsSlice := strings.Split(s[0], "-")
+	minDigits, _ := strconv.Atoi(digitsSlice[0])
+	maxDigits, _ := strconv.Atoi(digitsSlice[1])
+	alphabetSupported := strings.Split(s[1], ":")[0]
+	passwordString := s[2]
+
+	minWord := string(passwordString[minDigits-1]) == alphabetSupported
+	maxWord := string(passwordString[maxDigits-1]) == alphabetSupported
+
+	if minWord != maxWord {
+		return true
+	}
+
+	return false
+}
+
 func readFile() []string {
 	var input []string
 
-	file, _ := os.Open("./data.txt")
+	file, _ := os.Open("./sampledata.txt")
 	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
