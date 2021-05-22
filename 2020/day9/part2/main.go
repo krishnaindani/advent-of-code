5d641c32ded2b95be7ca0f564ce8c1e25aa29cfa
Krishna Indani <indanikrishna@gmail.com>
2021-05-21 20:47:21 -0700

    add optimized method

--- 2020/day9/part2/main.go
+++ 2020/day9/part2/main.go
@@ -5,13 +5,15 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"time"
 )
 
 func main() {
-
+	start := time.Now()
 	input := readInput()
 
 	fmt.Println(getNumberNotASumOfTwo(input, 25))
+	fmt.Println("execution time", time.Since(start))
 }
 
 func getNumberNotASumOfTwo(nums []int, preamble int) int {
@@ -50,6 +52,36 @@ func isNumberSumOfTwo(numsMap map[int]bool, num int) bool {
 	return false
 }
 
+//Time complexity: O(n). Though a one more O(n)
+//is required to calculate min and max
+func findContiguousSubArraySumV2(nums []int, target int) int {
+
+	currentSum := nums[0]
+	start := 0
+
+	for i := 1; i < len(nums); i++ {
+		for currentSum > target && start < i-1 {
+			currentSum = currentSum - nums[start]
+			start++
+		}
+
+		if currentSum == target {
+			//check for min+max here and then return the sum
+			currentMin := nums[start]
+			currentMax := nums[start]
+			for j := start + 1; j <= i; j++ {
+				currentMin = min(currentMin, nums[j])
+				currentMax = max(currentMax, nums[j])
+			}
+			return currentMin + currentMax
+		}
+		currentSum += nums[i]
+	}
+
+	return 0
+}
+
+//Time complexity: O(n2)
 func findContiguousSubArraySum(nums []int, target int) int {
 
 	for i := 0; i < len(nums); i++ {
