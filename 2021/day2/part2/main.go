3012a68c590c8f8b9868e221529a436acdc8935f
Krishna Indani <indanikrishna@gmail.com>
2021-12-15 13:46:15 -0800

    day two part two

---
+++ 2021/day2/part2/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	forward = "forward"
+	down    = "down"
+	up      = "up"
+)
+
+func main() {
+	directions, angles, err := readInput()
+	if err != nil {
+		fmt.Println("Read Input Error: ", err)
+		return
+	}
+
+	fmt.Println("Directions: ", directions)
+	fmt.Println("Angles: ", angles)
+
+	direction, angle := getDirection(directions, angles)
+	fmt.Println("direction: ", direction)
+	fmt.Println("angle: ", angle)
+	fmt.Println("Answer: ", direction*angle)
+}
+
+func getDirection(directions []string, angle []int) (int, int) {
+	var d, a, aim int
+
+	for i, v := range directions {
+		if v == forward {
+			d += angle[i]
+			a += aim * angle[i]
+		}
+		if v == down {
+			aim += angle[i]
+		}
+		if v == up {
+			aim -= angle[i]
+		}
+	}
+
+	return d, a
+}
+
+func readInput() ([]string, []int, error) {
+
+	var directions []string
+	var angle []int
+
+	file, err := os.Open("./data.txt")
+	if err != nil {
+		return nil, nil, err
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		s := strings.Split(scanner.Text(), " ")
+		directions = append(directions, s[0])
+		num, _ := strconv.Atoi(s[1])
+		angle = append(angle, num)
+	}
+
+	return directions, angle, nil
+}
